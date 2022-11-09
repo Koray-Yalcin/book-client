ac7dd6f20a8d022b7aef3798d420961fa95b22c5
Koray Yalçın <koray.25810@gmail.com>
2022-11-09 14:22:33 +0300

    commit

--- src/api/books.js
+++ src/api/books.js
@@ -10,11 +10,23 @@ export const deleteBooks = async (bookId) => {
     return await axios.delete(baseUrl + '/delete/' + bookId);
 }
 
-export const editBooks = async (bookId, name, author, price, image) => {
+export const editBooks = async (bookId, name, author, price, image, status) => {
     return await axios.put(baseUrl + '/update/' + bookId, {
         name: name,
         author: author,
         price: price,
-        image: image
+        image: image,
+        status: status
+    });
+}
+
+export const addBooks = async (name, author, price, status, image, userId) => {
+    return await axios.post(baseUrl , {
+        name: name,
+        author: author,
+        price: price,
+        image: image,
+        status: status,
+        user_id: userId
     });
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { logoutInitiate } from '../store/actions/userActions';
 import 'antd/dist/antd.css';
-import { Layout, message, Menu, Card, Button, Modal, Input } from 'antd';
+import { Layout, message, Menu, Card, Button, Modal, Input, Select } from 'antd';
 import './Home.css';
-import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
-import { getBooks, deleteBooks, editBooks } from '../api/books'
+import { EditOutlined, DeleteOutlined, BookOutlined } from '@ant-design/icons';
+import { getBooks, deleteBooks, editBooks, addBooks } from '../api/books'
 
-const { Header, Footer } = Layout;
+const { Header, Content, Footer } = Layout;
 const { Meta } = Card;
+const { Option } = Select;
 
 
 function Home() {
@@ -19,12 +20,9 @@ function Home() {
   const [bookState, setBookState] = useState([]);
   const [editBookState, setEditBookState] = useState([]);
   const [visibleState, setVisibleState] = useState(false);
-
-  const handleAuth = () => {
-    if(currentUser) {
-      dispatch(logoutInitiate());
-    }
-  }
+  const [visibleStateForAdd, setVisibleStateForAdd] = useState(false);
+  const [addBookState, setAddBookState] = useState([]);
+  const [keyState, setKeyState] = useState(1);
 
   useEffect(() => {
     console.log('geldi')
@@ -51,9 +49,9 @@ function Home() {
         }
       };
 
-    const editBook = async (bookId, name, author, price, image) => {
+    const editBook = async (bookId, name, author, price, image, status) => {
       try {
-        await editBooks(bookId, name, author, price, image).then(() => {
+        await editBooks(bookId, name, author, price, image, status).then(() => {
           getBooks().then((res) => {
           setBookState(res.data);
           message.success('Edited');
@@ -80,6 +78,36 @@ function Home() {
       }
     }
 
+    const onChangeAdd = (e) => {
+      if(e.target.name === 'name') {
+        setAddBookState({...addBookState, name: e.target.value})
+      }
+      else if(e.target.name === 'author') {
+        setAddBookState({...addBookState, author: e.target.value})
+      }
+      else if (e.target.name === 'price') {
+        setAddBookState({...addBookState, price: e.target.value})
+      }
+      else if (e.target.name === 'image') {
+        setAddBookState({...addBookState,image: e.target.value})
+      }
+    }
+
+    const addBook = async (name, author, price, status, image, userId) => {
+      try {
+        console.log(userId)
+        await addBooks(name, author, price, status, image, userId).then(() => {
+          getBooks().then((res) => {
+          setBookState(res.data);
+          message.success('Added');
+        });
+        })
+      } catch (error) {
+        message.error('Failed');
+        console.log(error);
+      }
+    }
+
   return (
     <div style={{textAlign: 'center'}}>
       <Layout>
@@ -92,8 +120,27 @@ function Home() {
     >
       <div className="logo" />
       <Menu
-      onClick={({key}) => {console.log(key)}}
+      onClick={({key}) => {
+         if(key === 'logout') {
+           if(currentUser) {
+            setKeyState(key);
+             dispatch(logoutInitiate());
+           }
+         } else if (key === '1') {
+          setKeyState(key);
+          console.log(keyState);
+         }
+         else if (key === '2') {
+          setKeyState(key);
+          console.log(keyState);
+         }
+         else if (key === '3') {
+          setKeyState(key);
+          console.log(keyState);
+         }
+      }}
         theme="dark"
+        style={{textAlign: 'center', justifyContent: 'center', display: 'flex'}}
         mode="horizontal"
         defaultSelectedKeys={['1']}
         items={[
@@ -111,24 +158,45 @@ function Home() {
             key: '3',
             label: 'Readed',
           },
+          {
+            label: currentUser?.displayName,
+            key: 'Profile',
+            icon: <BookOutlined />,
+            children: [
+              {
+                label: 'Logout',
+                key: 'logout'
+              }
+            ]
+
+          }
         ]}
       />
-      <button className='btn btn-danger' onClick={handleAuth}>Logout</button>
     </Header>
+    <Content
+      className="site-layout"
+      style={{
+        padding: '0 50px',
+        marginTop: 64,
+      }}
+    >
+      <Button style={{float: 'right', marginTop: '2%'}} type="primary" onClick={() => {setVisibleStateForAdd(true);}} >Add New Book</Button>
           <div style={{display: 'flex', marginLeft: '10%', flexWrap: 'wrap'}}>
             {bookState.map(book => 
             book.user_id === currentUser?.uid ?
+            book.status === parseInt(keyState) ?
+            
               <Card key={book._id}
               hoverable={false}
               style={{
-                width: 240, marginTop: '10%', marginRight: '3%'
+                width: 240, marginTop: '8%', marginRight: '3%'
               }}
               cover={<img style={{height: '330px'}} alt="example" src={book.image} />}
               >
                 <Meta style={{display: 'block', textAlign: 'center'}} title={book.name} description={book.author} />
                 <Meta style={{display: 'block', textAlign: 'center'}} title={book.price + ' TL'}  />
                 <Button style={{right: '40%'}}
-                  onClick={() => {setVisibleState(true); setEditBookState(book); console.log('onclick ',editBookState)}}
+                  onClick={() => {setVisibleState(true); setEditBookState(book);}}
                   type="primary"
                   icon={<EditOutlined />}
                   
@@ -137,13 +205,40 @@ function Home() {
                   <Modal
                   title= {'Edit Book'}
                   open={visibleState}
-                  onOk={() => {editBook(editBookState._id, editBookState.name, editBookState.author, editBookState.price, editBookState.image); setVisibleState(false);}}
-                  onCancel={() => {setVisibleState(false); setEditBookState([]); console.log('cancel ',editBookState)}}
+                  onOk={() => {editBook(editBookState._id, editBookState.name, editBookState.author, editBookState.price, editBookState.image, editBookState.status); setVisibleState(false);}}
+                  onCancel={() => {setVisibleState(false); setEditBookState([]);}}
                 >
                   <p>Book Name <Input placeholder="Enter Book Name" name='name' value={editBookState.name} onChange={(e) => {onChange(e);}} /></p>
                   <p>Author Name <Input placeholder="Enter Author Name" name='author' value={editBookState.author} onChange={(e) => {onChange(e);}} /></p>
                   <p> Price <Input placeholder="Enter Price" name='price' value={editBookState.price} onChange={(e) => {onChange(e);}} /></p>
+                  <p> Status </p>
+                    <Select onChange={(e) => {setEditBookState({...editBookState, status: e})}} value= {editBookState.status} >
+                        <Option disabled={true} value="0">Select Book Status</Option>
+                        <Option value="1">Reading</Option> 
+                        <Option value="2">Read Later</Option>
+                        <Option value="3">Readed</Option>  
+                    </Select>
                   <p> Image URL <Input placeholder="Enter Image URL" name='image' value={editBookState.image} onChange={(e) => {onChange(e);}} /></p>
+                </Modal>
+                
+                <Modal
+                  title= {'Add Book'}
+                  open={visibleStateForAdd}
+                  onOk={() => {addBook(addBookState.name, addBookState.author, addBookState.price, addBookState.status, addBookState.image, currentUser.uid); setVisibleStateForAdd(false);}}
+                  onCancel={() => {setVisibleStateForAdd(false);}}
+                >
+                  <p>Book Name <Input placeholder="Enter Book Name" name='name' value={addBookState.name} onChange={(e) => {onChangeAdd(e);}} /></p>
+                  <p>Author Name <Input placeholder="Enter Author Name" name='author' value={addBookState.author} onChange={(e) => {onChangeAdd(e);}} /></p>
+                  <p> Price <Input placeholder="Enter Price" name='price' value={addBookState.price} onChange={(e) => {onChangeAdd(e);}} /></p>
+                  <p> Status </p>
+                    <Select onChange={(e) => {setAddBookState({...addBookState, status: e})}} defaultValue="0">
+                      <Option disabled={true} value="0">Select Book Status</Option>
+                      <Option value="1">Reading</Option>
+                      <Option value="2">Read Later</Option>
+                      <Option value="3">Readed</Option>
+                    </Select>
+           
+                  <p> Image URL <Input placeholder="Enter Image URL" name='image' value={addBookState.image} onChange={(e) => {onChangeAdd(e);}} /></p>
                 </Modal>
 
                   <Button style={{left: '40%'}}
@@ -152,10 +247,11 @@ function Home() {
                   type="primary"
                   icon={<DeleteOutlined />}/>
               
-              </Card> : null)
+              </Card> : null : null)
               }
   
           </div>
+          </Content>
     <Footer
       style={{
         textAlign: 'center',
